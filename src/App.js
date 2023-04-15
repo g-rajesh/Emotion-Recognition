07f8fab18711e5fcdcc4ed3e38f8ffe6aeab5b80
g-rajesh <coding3601@gmail.com>
2023-04-15 19:08:19 +0530

    Emotion Recognition :: PDF Generation Compelted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Upload from "./components/Upload";
+import Result from "./components/Result";
+import Download from "./components/Download";
+import { useState } from "react";
 
-function App() {
+
+const initialData = {
+  title: "",
+  fullName: "",
+  dob: "",
+  mno: ""
+};
+const App = () => {
+  const [prediction, setPrediction] = useState({});
+  const [user, setUser] = useState(initialData);
+
+  const handleChange = (e) => {
+    setUser({...user, [e.target.name]: e.target.value});
+  }
+
+  const clearData = () => {
+    setPrediction({});
+    setUser(initialData);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Upload setPrediction={setPrediction} />} />
+      <Route path="/result" element={<Result prediction={prediction} user={user} handleChange={handleChange} />} />
+      <Route path="/download" element={<Download user={user} prediction={prediction} clearData={clearData} />} />
+    </Routes>
   );
 }
 
